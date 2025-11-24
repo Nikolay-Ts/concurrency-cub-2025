2ba4cd78c665706fe720c26ed2cdae5a88b3e5d7
Nik Tsonev <nikolay.l.tsonev@gmail.com>
2025-11-24 20:13:25 +0100

    finished day 6 and 7

--- src/day6/ConcurrentHashTable.kt
+++ src/day6/ConcurrentHashTable.kt
@@ -3,23 +3,17 @@
 package day6
 
 import java.util.concurrent.atomic.*
+import kotlin.math.absoluteValue
 
 class ConcurrentHashTable<K : Any, V : Any>(initialCapacity: Int) {
     private val table = AtomicReference(Table<K, V>(initialCapacity))
 
     fun put(key: K, value: V): V? {
         while (true) {
-            // Try to insert the key/value pair.
             val putResult = table.get().put(key, value)
             if (putResult === NEEDS_REHASH) {
-                // The current table is too small to insert a new key.
-                // Create a new table of x2 capacity,
-                // copy all elements to it,
-                // and restart the current operation.
                 resize()
             } else {
-                // The operation has been successfully performed,
-                // return the previous value associated with the key.
                 return putResult as V?
             }
         }
@@ -34,25 +28,168 @@ class ConcurrentHashTable<K : Any, V : Any>(initialCapacity: Int) {
     }
 
     private fun resize() {
-        // TODO: Implement me!
+        val currentTable = table.get()
+        val newCapacity = currentTable.capacity * 2
+        val newTable = Table<K, V>(newCapacity)
+
+        for (i in 0 until currentTable.capacity) {
+            val key = currentTable.keys.get(i)
+            val value = currentTable.values.get(i)
+
+            if (key != null && key !== EMPTY && key !== TOMBSTONE) {
+                @Suppress("UNCHECKED_CAST")
+                val actualKey = key as K
+                if (value != null && value !== TOMBSTONE) {
+                    @Suppress("UNCHECKED_CAST")
+                    val actualValue = value
+                    newTable.putForResize(actualKey, actualValue)
+                }
+            }
+        }
+        table.compareAndSet(currentTable, newTable)
     }
 
     class Table<K : Any, V : Any>(val capacity: Int) {
         val keys = AtomicReferenceArray<Any?>(capacity)
         val values = AtomicReferenceArray<V?>(capacity)
 
+        private val size = AtomicInteger(0)
+        private val threshold = (capacity * 0.75).toInt()
+
+        init {
+            for (i in 0 until capacity) {
+                keys.set(i, EMPTY)
+                values.set(i, null)
+            }
+        }
+
         fun put(key: K, value: V): Any? {
-            TODO("Implement me!")
+            val hash = key.hashCode().absoluteValue
+            var index = hash % capacity
+            var firstTombstone = -1
+
+            for (attempt in 0 until capacity) {
+                val currentKey = keys.get(index)
+
+                when (currentKey) {
+                    EMPTY -> {
+                        if (firstTombstone != -1) firstTombstone else index
+                        if (firstTombstone == -1) {
+                            if (keys.compareAndSet(index, EMPTY, key)) {
+                                values.set(index, value)
+                                if (size.incrementAndGet() > threshold) {
+                                    return NEEDS_REHASH
+                                }
+                                return null
+                            }
+                        } else {
+                            val tombstoneKey = keys.get(firstTombstone)
+                            if (tombstoneKey !== TOMBSTONE) {
+                                return put(key, value)
+                            }
+
+                            if (keys.compareAndSet(firstTombstone, TOMBSTONE, key)) {
+                                values.set(firstTombstone, value)
+                                size.incrementAndGet()
+                                return null
+                            }
+                            return put(key, value)
+                        }
+                    }
+                    TOMBSTONE -> {
+                        if (firstTombstone == -1) {
+                            firstTombstone = index
+                        }
+                    }
+                    key -> {
+                        val oldValue = values.getAndSet(index, value)
+                        return oldValue
+                    }
+                    else -> { }
+                }
+
+                index = (index + 1) % capacity
+            }
+
+            return NEEDS_REHASH
         }
 
         fun get(key: K): V? {
-            TODO("Implement me!")
+            val hash = key.hashCode().absoluteValue
+            var index = hash % capacity
+
+            for (attempt in 0 until capacity) {
+                val currentKey = keys.get(index)
+
+                when (currentKey) {
+                    EMPTY -> return null
+                    key -> {
+                        val value = values.get(index)
+                        return if (value === TOMBSTONE) null else value as V?
+                    }
+                    TOMBSTONE -> { }
+                }
+
+                index = (index + 1) % capacity
+            }
+
+            return null
         }
 
         fun remove(key: K): V? {
-            TODO("Implement me!")
+            val hash = key.hashCode().absoluteValue
+            var index = hash % capacity
+
+            for (attempt in 0 until capacity) {
+                val currentKey = keys.get(index)
+
+                when (currentKey) {
+                    EMPTY -> return null
+                    key -> {
+                        val oldValue = values.get(index)
+                        if (oldValue !== TOMBSTONE) {
+                            // Mark value as tombstone
+                            if (values.compareAndSet(index, oldValue, TOMBSTONE as V?)) {
+                                keys.compareAndSet(index, key, TOMBSTONE)
+                                size.decrementAndGet()
+                                return oldValue
+                            }
+                            return remove(key)
+                        }
+                        return null
+                    }
+                    TOMBSTONE -> { }
+                }
+
+                index = (index + 1) % capacity
+            }
+
+            return null
+        }
+
+        internal fun putForResize(key: K, value: V) {
+            val hash = key.hashCode().absoluteValue
+            var index = hash % capacity
+
+            for (attempt in 0 until capacity) {
+                val currentKey = keys.get(index)
+
+                if (currentKey === EMPTY || currentKey === TOMBSTONE) {
+                    if (keys.compareAndSet(index, currentKey, key)) {
+                        values.set(index, value)
+                        return
+                    }
+                } else if (currentKey == key) {
+                    values.set(index, value)
+                    return
+                }
+
+                index = (index + 1) % capacity
+            }
         }
     }
 }
 
 private val NEEDS_REHASH = Any()
+private val EMPTY = Any()
+private val TOMBSTONE = Any()
--- src/day7/Dijkstra.kt
+++ src/day7/Dijkstra.kt
@@ -1,40 +1,56 @@
 package day7
 
-import java.util.*
-import java.util.concurrent.*
-import kotlin.concurrent.*
+import java.util.Comparator
+import java.util.concurrent.Phaser
+import java.util.concurrent.PriorityBlockingQueue
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.concurrent.thread
 
-private val NODE_DISTANCE_COMPARATOR = Comparator<Node> { o1, o2 -> Integer.compare(o1!!.distance, o2!!.distance) }
+private val NODE_DISTANCE_COMPARATOR =
+    Comparator<Node> { o1, o2 -> o1.distance.compareTo(o2.distance) }
 
-// Returns `Integer.MAX_VALUE` if a path has not been found.
 fun shortestPathParallel(start: Node) {
     val workers = Runtime.getRuntime().availableProcessors()
-    // The distance to the start node is `0`
     start.distance = 0
-    // Create a priority (by distance) queue and add the start node into it
-    val q = PriorityQueue(workers, NODE_DISTANCE_COMPARATOR) // TODO replace me with a multi-queue based PQ!
+
+    val q = PriorityBlockingQueue<Node>(11, NODE_DISTANCE_COMPARATOR)
     q.add(start)
-    // Run worker threads and wait until the total work is done
-    val onFinish = Phaser(workers + 1) // `arrive()` should be invoked at the end by each worker
+
+    val active = AtomicInteger(1)
+    val onFinish = Phaser(workers + 1)
+
     repeat(workers) {
         thread {
             while (true) {
-                // TODO Write the required algorithm here,
-                // TODO break from this loop when there is no more node to process.
-                // TODO Be careful, "empty queue" != "all nodes are processed".
-//                val cur: Node? = synchronized(q) { q.poll() }
-//                if (cur == null) {
-//                    if (workIsDone) break else continue
-//                }
-//                for (e in cur.outgoingEdges) {
-//                    if (e.to.distance > cur.distance + e.weight) {
-//                        e.to.distance = cur.distance + e.weight
-//                        q.addOrDecreaseKey(e.to)
-//                    }
-//                }
+                val cur = q.poll()
+
+                if (cur == null) {
+                    if (active.get() == 0) {
+                        break
+                    }
+                    Thread.yield()
+                    continue
+                }
+                for (e in cur.outgoingEdges) {
+                    while (true) {
+                        val oldDistance = e.to.distance
+                        val newDistance = cur.distance + e.weight
+
+                        if (oldDistance <= newDistance) {
+                            break
+                        }
+                        if (e.to.casDistance(oldDistance, newDistance)) {
+                            q.add(e.to)
+                            active.incrementAndGet()
+                            break
+                        }
+                    }
+                }
+                active.decrementAndGet()
             }
             onFinish.arrive()
         }
     }
+
     onFinish.arriveAndAwaitAdvance()
 }
